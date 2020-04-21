904706538112b9733321b7d9a46c9db3f872d832
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-21 03:06:49 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,11 @@ class Pokemon
   
   attr_accessor :id, :name, :type, :db
   
-  def initialize(id=nil, name, type)
+  def initialize(id=nil, name, type, db)
     @id = id 
     @name  =  name
     @type = type
+    @db = db
   end
   
 end
